2a7e8cb9699e16d58fa93f9f97269f730bca187b
buiquangkhaiinfo <122881045+buiquangkhaiinfo@users.noreply.github.com>
2023-01-18 10:41:26 +0700

    Add files via upload

---
+++ js/music-list.js
@@ -0,0 +1,67 @@
+let allMusic = [{
+        name: "Nhạc Tẩm Đá",
+        artist: "DJ Bùi Quang Khải",
+        img: "1",
+        src: "1"
+    },
+    {
+        name: "Tết Đong Đầy",
+        artist: "Bùi Quang Khải",
+        img: "2",
+        src: "2"
+    },
+    {
+        name: "Năm Mới Bình An",
+        artist: "Bùi Quang Khải",
+        img: "3",
+        src: "3"
+    },
+    {
+        name: "Nghe Là Phê",
+        artist: "DJ Bùi Quang Khải",
+        img: "4",
+        src: "4"
+    },
+    {
+        name: "Chờ Đợi Có Đáng Sợ",
+        artist: "Bùi Quang Khải",
+        img: "5",
+        src: "5"
+    },
+    {
+        name: "Ánh Chiều Tàn Cover",
+        artist: "Bùi Quang Khải",
+        img: "6",
+        src: "6"
+    },
+    {
+        name: "Anh Yêu Em",
+        artist: "Bùi Quang Khải",
+        img: "7",
+        src: "7"
+    },
+    {
+        name: "Phải Làm Sao Để Níu Kéo Một Người ?",
+        artist: "Bùi Quang Khải",
+        img: "8",
+        src: "8"
+    },
+    {
+        name: "Don't Côi",
+        artist: "Bùi Quang Khải Dev",
+        img: "9",
+        src: "9"
+    },
+    {
+        name: "Nhạc Này Dz Vayy",
+        artist: "Bùi Quang Khải",
+        img: "10",
+        src: "10"
+    },
+    {
+        name: "I Love Trung Học Cơ Sở Phước Tân 1 <3 ",
+        artist: "Bùi Quang Khải",
+        img: "11",
+        src: "11"
+    },
+];
